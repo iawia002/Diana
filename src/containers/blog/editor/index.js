86ac540424d7de0daa81a9a67d08fd5c18c846e8
iawia002 <z2d@jifangcheng.com>
2017-09-08 16:35:52 +0800

    fix editor [ci skip]

--- src/containers/blog/editor/index.js
+++ src/containers/blog/editor/index.js
@@ -1,17 +1,19 @@
-'use strict';
+/* eslint-env browser */
 
 import 'codemirror/lib/codemirror.css';
-import 'codemirror/mode/gfm/gfm.js';
 import 'codemirror/theme/solarized.css';
 
-import '../../../css/editor.scss';
 import 'font-awesome/css/font-awesome.css';
 import 'highlight.js/styles/solarized-dark.css';
 
 import $ from 'jquery';
 import key from 'keymaster';
 import CodeMirror from 'codemirror';
-import Editor from './editor.js'
+import 'codemirror/mode/gfm/gfm';
+
+import Editor from './editor';
+
+import '../../../css/editor.scss';
 
 
 const editor = new Editor();
@@ -33,7 +35,10 @@ codeEditor.on('change', (instance, changeObj) => {
 
 codeEditor.on('scroll', (instance) => {
   const scrollInfo = instance.getScrollInfo();
-  const percentage = (scrollInfo.top + scrollInfo.clientHeight) / scrollInfo.height;
+  let percentage = 0;
+  if (scrollInfo.top !== 0) {
+    percentage = (scrollInfo.top + scrollInfo.clientHeight) / scrollInfo.height;
+  }
   const outputHeight = $('#output')[0].scrollHeight - $('#output')[0].offsetHeight;
   $('#output').scrollTop(percentage * outputHeight);
 });
